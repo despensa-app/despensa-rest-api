4100e0941df30cc9acc8307ab4a13a4260d878e4
Nicolás Marulanda P <nmarulo@outlook.com>
2023-12-02 18:21:10 +0100

    Agregada clase para el "RequestScope"

---
+++ src/main/java/dev/nmarulo/depensaapp/commons/component/DataRequestScope.java
@@ -0,0 +1,35 @@
+package dev.nmarulo.depensaapp.commons.component;
+
+import dev.nmarulo.depensaapp.commons.classes.PagingAndSortingReq;
+import dev.nmarulo.depensaapp.configuration.AppProperties;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.LocaleResolver;
+
+@RequestScope
+@Component
+@RequiredArgsConstructor
+@Data
+public class DataRequestScope {
+    
+    private Pageable pageable;
+    
+    private PagingAndSortingReq pagingAndSortingReq;
+    
+    private final AppProperties appProperties;
+    
+    private final LocaleResolver localeResolver;
+    
+    public Pageable getPageable() {
+        if (this.pageable == null) {
+            this.pageable = PageRequest.of(0, this.appProperties.getPageableSize());
+        }
+        
+        return pageable;
+    }
+    
+}
