f9fa53bdad6ece025962f736c4fbbabcbed634d2
Nicolás Marulanda P <nmarulo@outlook.com>
2024-07-03 22:26:43 +0200

    Modificado: UserRepository - Buscar por nombre de usuario.

--- src/main/java/dev/nmarulo/depensaapp/app/users/UserRepository.java
+++ src/main/java/dev/nmarulo/depensaapp/app/users/UserRepository.java
@@ -3,5 +3,11 @@ package dev.nmarulo.depensaapp.app.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+    Optional<User> findByUsername(String username);
+    
+}
