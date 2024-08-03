8a7a2063e1bfabaea16ea71509d5859099109298
Nicolás Marulanda P <nmarulo@outlook.com>
2024-08-03 22:48:00 +0200

    Configuración OAuth2ResourceServer - Se establece el AutenticaciónConverter personalizado.

--- src/main/java/dev/nmarulo/depensaapp/configuration/WebSecurityConfig.java
+++ src/main/java/dev/nmarulo/depensaapp/configuration/WebSecurityConfig.java
@@ -5,13 +5,18 @@ import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import dev.nmarulo.depensaapp.app.users.UserRepository;
+import dev.nmarulo.depensaapp.commons.component.LocalMessage;
+import dev.nmarulo.depensaapp.commons.converter.CustomJwtAuthenticationConverter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,6 +40,10 @@ public class WebSecurityConfig {
     
     private final AppProperties appProperties;
     
+    private final UserRepository userRepository;
+    
+    private final LocalMessage localeMessage;
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.securityMatcher(appProperties.getPathPrefix() + "/**")
@@ -50,6 +59,7 @@ public class WebSecurityConfig {
             .httpBasic(AbstractHttpConfigurer::disable)
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .oauth2ResourceServer(configOAuth2())
             .cors(value -> value.configurationSource(corsConfigurationSource()));
         
         return http.build();
@@ -87,6 +97,12 @@ public class WebSecurityConfig {
         return NimbusJwtDecoder.withSecretKey(macSigner.getSecretKey())
                                .macAlgorithm(MacAlgorithm.HS256)
                                .build();
+    }
+    
+    private Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> configOAuth2() {
+        final var converter = new CustomJwtAuthenticationConverter(this.userRepository, this.localeMessage);
+        
+        return oauth -> oauth.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(converter));
     }
     
 }
