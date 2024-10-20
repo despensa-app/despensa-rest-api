004f33d8ab5477dd10541b85b120fc0c07a882ea
Nicolás Marulanda P <nmarulo@outlook.com>
2024-10-20 22:00:26 +0200

    Refactorizado: Clase ProductController - Intento que sea algo más legible de leer. - El "@Getter" no es necesario

--- src/main/java/dev/nmarulo/depensaapp/app/products/ProductController.java
+++ src/main/java/dev/nmarulo/depensaapp/app/products/ProductController.java
@@ -6,7 +6,6 @@ import dev.nmarulo.depensaapp.app.products.dtos.SaveShoppingListProductReq;
 import dev.nmarulo.depensaapp.app.products.dtos.SaveShoppingListProductRes;
 import dev.nmarulo.depensaapp.commons.component.DataRequestScope;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -16,11 +15,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-@Getter
 @Tag(name = "Product", description = "Endpoints for managing products")
 public class ProductController {
     
-    private final ProductService service;
+    private final ProductService productService;
     
     private final DataRequestScope dataRequestScope;
     
@@ -29,21 +27,23 @@ public class ProductController {
                                                                                          @RequestParam(value = "exclude",
                                                                                                        required = false) boolean isExclude,
                                                                                          @PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(this.service.findAllProductsByShoppingListId(shoppingListId,
-                                                                              isExclude,
-                                                                              pageable,
-                                                                              this.dataRequestScope.getAuthenticationPrincipal()));
+        final var response = this.productService.findAllProductsByShoppingListId(shoppingListId,
+                                                                                 isExclude,
+                                                                                 pageable,
+                                                                                 this.dataRequestScope.getAuthenticationPrincipal());
+        
+        return ResponseEntity.ok(response);
     }
     
     @PostMapping("/shopping-list")
     public ResponseEntity<SaveShoppingListProductRes> saveProductInShoppingList(@RequestBody SaveShoppingListProductReq request) {
-        return ResponseEntity.ok(this.service.saveProductInShoppingList(request,
-                                                                        this.dataRequestScope.getAuthenticationPrincipal()));
+        return ResponseEntity.ok(this.productService.saveProductInShoppingList(request,
+                                                                               this.dataRequestScope.getAuthenticationPrincipal()));
     }
     
     @GetMapping
     public ResponseEntity<FindAllProductRes> findAll(@PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(this.service.findAll(pageable));
+        return ResponseEntity.ok(this.productService.findAll(pageable));
     }
     
 }
