bf66ce83799fb70900ffe896e678b5c883da2258
Nicolás Marulanda P <nmarulo@outlook.com>
2024-10-20 22:01:59 +0200

    Refactorizado: Clase ShoppingListController - El "@Getter" no es necesario. - Cambios menores y formateo de código.

--- src/main/java/dev/nmarulo/depensaapp/app/shoppinglist/ShoppingListController.java
+++ src/main/java/dev/nmarulo/depensaapp/app/shoppinglist/ShoppingListController.java
@@ -3,7 +3,6 @@ package dev.nmarulo.depensaapp.app.shoppinglist;
 import dev.nmarulo.depensaapp.app.shoppinglist.dtos.*;
 import dev.nmarulo.depensaapp.commons.component.DataRequestScope;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,36 +12,37 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/shopping-lists")
 @RequiredArgsConstructor
-@Getter
 @Tag(name = "Shopping List", description = "Endpoints for managing shopping lists")
 public class ShoppingListController {
     
-    private final ShoppingListService service;
+    private final ShoppingListService shoppingListService;
     
     private final DataRequestScope dataRequestScope;
     
     @GetMapping
     public ResponseEntity<FindAllShoppingListRes> findAll(@PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(this.service.findAll(pageable, this.dataRequestScope.getAuthenticationPrincipal()));
+        return ResponseEntity.ok(this.shoppingListService.findAll(pageable,
+                                                                  this.dataRequestScope.getAuthenticationPrincipal()));
     }
     
     @GetMapping("/{id}")
     public ResponseEntity<FindByIdShoppingListRes> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(this.service.findById(id, this.dataRequestScope.getAuthenticationPrincipal()));
+        return ResponseEntity.ok(this.shoppingListService.findById(id,
+                                                                   this.dataRequestScope.getAuthenticationPrincipal()));
     }
     
     @GetMapping("/{id}/products/{productId}")
     public ResponseEntity<FindByIdProductShoppingListRest> findByIdProduct(@PathVariable Integer id,
                                                                            @PathVariable Long productId) {
-        return ResponseEntity.ok(this.service.findByIdProduct(id,
-                                                              productId,
-                                                              this.dataRequestScope.getAuthenticationPrincipal()));
+        return ResponseEntity.ok(this.shoppingListService.findByIdProduct(id,
+                                                                          productId,
+                                                                          this.dataRequestScope.getAuthenticationPrincipal()));
     }
     
     @DeleteMapping("/{id}/products")
     public ResponseEntity<?> deleteProducts(@PathVariable Integer id,
                                             @RequestBody DeleteProductsShoppingListReq request) {
-        this.service.deleteProducts(id, request, this.dataRequestScope.getAuthenticationPrincipal());
+        this.shoppingListService.deleteProducts(id, request, this.dataRequestScope.getAuthenticationPrincipal());
         
         return ResponseEntity.noContent()
                              .build();
@@ -50,18 +50,21 @@ public class ShoppingListController {
     
     @PostMapping
     public ResponseEntity<SaveShoppingListRes> save(@RequestBody SaveShoppingListReq request) {
-        return ResponseEntity.ok(this.service.save(request, this.dataRequestScope.getAuthenticationPrincipal()));
+        return ResponseEntity.ok(this.shoppingListService.save(request,
+                                                               this.dataRequestScope.getAuthenticationPrincipal()));
     }
     
     @PutMapping("/{id}")
     public ResponseEntity<UpdateShoppingListRes> update(@PathVariable Integer id,
                                                         @RequestBody UpdateShoppingListReq request) {
-        return ResponseEntity.ok(this.service.update(id, request, this.dataRequestScope.getAuthenticationPrincipal()));
+        return ResponseEntity.ok(this.shoppingListService.update(id,
+                                                                 request,
+                                                                 this.dataRequestScope.getAuthenticationPrincipal()));
     }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
-        this.service.delete(id, this.dataRequestScope.getAuthenticationPrincipal());
+        this.shoppingListService.delete(id, this.dataRequestScope.getAuthenticationPrincipal());
         
         return ResponseEntity.noContent()
                              .build();
