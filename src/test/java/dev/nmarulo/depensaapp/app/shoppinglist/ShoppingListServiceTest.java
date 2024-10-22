6dae1148a4f7e83fcf0ab12e242084c49373b6fd
Nicolás Marulanda P <nmarulo@outlook.com>
2024-10-22 23:13:16 +0200

    Testing: Clase ShoppingListService - Test básico para el método "testFindAll".

---
+++ src/test/java/dev/nmarulo/depensaapp/app/shoppinglist/ShoppingListServiceTest.java
@@ -0,0 +1,46 @@
+package dev.nmarulo.depensaapp.app.shoppinglist;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ShoppingListServiceTest {
+    
+    private static ShoppingListServiceTestUtil shoppingListServiceTestUtil;
+    
+    @InjectMocks
+    private ShoppingListService shoppingListService;
+    
+    @Mock
+    private Pageable pageable;
+    
+    @Mock
+    private ShoppingListRepository shoppingListRepository;
+    
+    @BeforeAll
+    static void beforeAll() {
+        shoppingListServiceTestUtil = new ShoppingListServiceTestUtil();
+    }
+    
+    @Test
+    void testFindAll() {
+        final var expected = shoppingListServiceTestUtil.getFindAllShoppingListRes();
+        final var user = shoppingListServiceTestUtil.getUser();
+        final var shoppingListPage = shoppingListServiceTestUtil.getShoppingListPage();
+        
+        when(shoppingListRepository.findAllByUser(user, pageable)).thenReturn(shoppingListPage);
+        
+        final var actual = shoppingListService.findAll(this.pageable, user);
+        
+        assertEquals(expected, actual);
+    }
+    
+}
---
+++ src/test/java/dev/nmarulo/depensaapp/app/shoppinglist/ShoppingListServiceTestUtil.java
@@ -0,0 +1,89 @@
+package dev.nmarulo.depensaapp.app.shoppinglist;
+
+import dev.nmarulo.depensaapp.FakeTestUtil;
+import dev.nmarulo.depensaapp.app.shoppinglist.dtos.FindAllShoppingListRes;
+import dev.nmarulo.depensaapp.app.users.User;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public class ShoppingListServiceTestUtil {
+    
+    private final FindAllShoppingListRes findAllShoppingListRes;
+    
+    private final User user;
+    
+    private final Page<ShoppingList> shoppingListPage;
+    
+    public ShoppingListServiceTestUtil() {
+        this.user = initUser();
+        final var shoppingList = initShoppingList(this.user);
+        this.shoppingListPage = initShoppingListPage(shoppingList);
+        this.findAllShoppingListRes = initFindAllShoppingListRes(this.shoppingListPage);
+    }
+    
+    private User initUser() {
+        final var user = new User();
+        
+        user.setId(FakeTestUtil.randomLong());
+        user.setUsername(FakeTestUtil.randomUsername());
+        user.setPassword(FakeTestUtil.randomPassword());
+        user.setEmail(FakeTestUtil.randomEmail());
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        user.setShoppingLists(Collections.emptySet());
+        
+        return user;
+    }
+    
+    private ShoppingList initShoppingList(User user) {
+        ShoppingList shoppingList = new ShoppingList();
+        
+        shoppingList.setId(FakeTestUtil.randomInteger());
+        shoppingList.setName(FakeTestUtil.randomSentence());
+        shoppingList.setTotalProducts(0);
+        shoppingList.setTotalCalories(BigDecimal.ZERO);
+        shoppingList.setTotalPrice(BigDecimal.ZERO);
+        shoppingList.setCreatedAt(LocalDateTime.now());
+        shoppingList.setUpdatedAt(LocalDateTime.now());
+        shoppingList.setProductHasShoppingList(Collections.emptySet());
+        shoppingList.setUser(user);
+        
+        return shoppingList;
+    }
+    
+    private Page<ShoppingList> initShoppingListPage(ShoppingList shoppingList) {
+        List<ShoppingList> content = Collections.singletonList(shoppingList);
+        final var pageable = PageRequest.of(0, 10);
+        
+        return new PageImpl<>(content, pageable, 1);
+    }
+    
+    private FindAllShoppingListRes initFindAllShoppingListRes(Page<ShoppingList> shoppingListPage) {
+        final var findAllShoppingListRes = new FindAllShoppingListRes();
+        final var shoppingListRest = new FindAllShoppingListRes.ShoppingList();
+        final var shoppingList = shoppingListPage.getContent()
+                                                 .getFirst();
+        
+        shoppingListRest.setId(shoppingList.getId());
+        shoppingListRest.setName(shoppingList.getName());
+        shoppingListRest.setTotalProducts(shoppingList.getTotalProducts());
+        shoppingListRest.setCreatedAt(shoppingList.getCreatedAt());
+        
+        findAllShoppingListRes.setCurrentPage(shoppingListPage.getNumber());
+        findAllShoppingListRes.setPageSize(shoppingListPage.getNumberOfElements());
+        findAllShoppingListRes.setTotalPages(shoppingListPage.getTotalPages());
+        findAllShoppingListRes.setTotal(shoppingListPage.getTotalElements());
+        findAllShoppingListRes.setContent(Collections.singletonList(shoppingListRest));
+        
+        return findAllShoppingListRes;
+    }
+    
+}
