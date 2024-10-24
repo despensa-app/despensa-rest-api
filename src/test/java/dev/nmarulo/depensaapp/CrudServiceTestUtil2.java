c511c4d9411e178ce2624bdb59fb91e9aaa3af19
Nicolás Marulanda P. <nmarulo@outlook.com>
2024-10-24 22:06:41 +0200

    up

---
+++ src/test/java/dev/nmarulo/depensaapp/CrudServiceTestUtil2.java
@@ -0,0 +1,58 @@
+package dev.nmarulo.depensaapp;
+
+import lombok.Getter;
+import lombok.Value;
+
+import java.util.List;
+
+@Getter
+public abstract class CrudServiceTestUtil2<E, R, T, ID> {
+    
+    private E request;
+    
+    private R response;
+    
+    private T entity;
+    
+    private T entitySaveResult;
+    
+    private ID id;
+    
+    private final List<R> responseList;
+    
+    private final List<T> entityList;
+    
+    public CrudServiceTestUtil() {
+        setInit();
+        this.responseList = List.of(this.response);
+        this.entityList = List.of(this.entity);
+    }
+    
+    public abstract Init<E, R, T, ID> init();
+    
+    private void setInit() {
+        Init<E, R, T, ID> init = init();
+        
+        this.request = init.getRequest();
+        this.response = init.getResponse();
+        this.entity = init.getEntity();
+        this.entitySaveResult = init.getEntitySaveResult();
+        this.id = init.getId();
+    }
+    
+    @Value(staticConstructor = "of")
+    public static class Init<E, R, T, ID> {
+        
+        E request;
+        
+        R response;
+        
+        T entity;
+        
+        T entitySaveResult;
+        
+        ID id;
+        
+    }
+    
+}
