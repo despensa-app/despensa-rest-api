553d7f98e28bc7f3788f58491a4b40c20123e66d
Nicolás Marulanda P. <nmarulo@outlook.com>
2024-10-24 21:56:46 +0200

    up

--- src/main/java/dev/nmarulo/depensaapp/commons/Test.java
+++ src/main/java/dev/nmarulo/depensaapp/commons/Test.java
@@ -3,7 +3,36 @@ package dev.nmarulo.depensaapp.commons;
 public abstract class Test {
 
     public static void test() {
+        int a = 0;
+    }
 
+    public void test0() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            
+            return;
+        }
+    }
+    
+    public void test1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            System.out.println("test0" + i);
+            
+            return;
+        }
     }
     
 }
