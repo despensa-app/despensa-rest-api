281f23b91e6b5845ba4dfd29845acff8027589bf
Nicolás Marulanda P. <nmarulo@outlook.com>
2024-10-25 20:57:56 +0200

    up

---
+++ src/test/java/dev/nmarulo/depensaapp/app/Test001.java
@@ -0,0 +1,14 @@
+package dev.nmarulo.depensaapp;
+
+import lombok.Getter;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@Getter
+class TypeDishServiceTest {
+    
+    
+}
