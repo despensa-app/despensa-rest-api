2dbdafc7ea0d1eb585bae9489c7216f36883bfb4
Nicolás Marulanda P <nmarulo@outlook.com>
2024-11-22 00:21:05 +0100

    Modificado: Clases de utilidades - Nueva clase "IntegerUtil". - Nuevos métodos para la clase "BigDecimalUtil".

--- src/main/java/dev/nmarulo/depensaapp/commons/util/BigDecimalUtil.java
+++ src/main/java/dev/nmarulo/depensaapp/commons/util/BigDecimalUtil.java
@@ -1,6 +1,7 @@
 package dev.nmarulo.depensaapp.commons.util;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicReference;
 
 public final class BigDecimalUtil {
     
@@ -8,7 +9,23 @@ public final class BigDecimalUtil {
     }
     
     public static BigDecimal multiply(BigDecimal bigDecimal, Integer integer) {
-        return bigDecimal.multiply(new BigDecimal(integer));
+        return bigDecimal.multiply(valueOf(integer));
+    }
+    
+    public static BigDecimal valueOf(Integer integer) {
+        if (integer == null) {
+            return BigDecimal.ZERO;
+        }
+        
+        return new BigDecimal(integer);
+    }
+    
+    public static BigDecimal nullToZero(BigDecimal bigDecimal) {
+        return bigDecimal == null ? BigDecimal.ZERO : bigDecimal;
+    }
+    
+    public static AtomicReference<BigDecimal> newAtomicReference(BigDecimal bigDecimal) {
+        return new AtomicReference<>(nullToZero(bigDecimal));
     }
     
 }
