a88b74b6214060b19ea5b0b7c0b4bfb104fbc2af
Nicolás Marulanda P <nmarulo@outlook.com>
2024-11-27 22:43:26 +0100

    Nueva clase "SelectOption" - Representa las opciones de un elemento "select" HTML.

---
+++ src/main/java/dev/nmarulo/depensaapp/app/shoppinglist/dtos/SelectOption.java
@@ -0,0 +1,24 @@
+package dev.nmarulo.depensaapp.app.shoppinglist.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SelectOption<V, L> {
+    
+    private V value;
+    
+    private L label;
+    
+    private boolean selected;
+    
+    public SelectOption(V value, L label) {
+        this.value = value;
+        this.label = label;
+        this.selected = false;
+    }
+    
+}
