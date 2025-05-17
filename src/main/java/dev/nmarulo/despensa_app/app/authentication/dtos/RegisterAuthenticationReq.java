271158683acb8fa3753cfe6e70e77b48e5d238d1
Nicolás Marulanda P <nmarulo@outlook.com>
2025-05-17 19:35:00 +0200

    Actualizado DTO de la api "/auth/register"
    Olvide añadir las anotaciones de swagger.

--- src/main/java/dev/nmarulo/despensa_app/app/authentication/dtos/RegisterAuthenticationReq.java
+++ src/main/java/dev/nmarulo/despensa_app/app/authentication/dtos/RegisterAuthenticationReq.java
@@ -1,12 +1,15 @@
 package dev.nmarulo.despensa_app.app.authentication.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class RegisterAuthenticationReq {
     
+    @Schema(example = "newUser", requiredMode = Schema.RequiredMode.REQUIRED)
     private String username;
     
+    @Schema(example = "password123", requiredMode = Schema.RequiredMode.REQUIRED)
     private String password;
     
 }
